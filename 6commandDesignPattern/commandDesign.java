6e575a4560eb219d253cdd8f51362d37dcc0b767
VICKYJHA00 <try.vickyjha@gmail.com>
2026-07-06 13:38:11 +0530

    command design pattern

--- 6commandDesignPattern/commandDesign.java
+++ 6commandDesignPattern/commandDesign.java
@@ -49,5 +49,83 @@ class lightCommand implements Command{
 
 }
 
+class fanCommand implements Command{
+    private fan f;
+
+    public fanCommand(fan f){
+        this.f = f;
+    }
+
+    public void execute(){
+        f.on();
+    }
+
+    public void undo(){
+        f.off();
+    }
+}
+
+class remoteContoller{
+    private static final int newButton = 4;
+    private Command[] buttons;
+    private boolean[] buttonpressed;
+
+    public remoteContoller(){
+        buttons = new Command[newButton];
+        buttonpressed = new boolean[newButton];
+        for(int i = 0;i<newButton;i++){
+            buttons[i] = null;
+            buttonpressed[i] = false;
+        }
+    }
+
+    public void setCommand(int idx,Command cmd){
+        if(idx>= 0 && idx<newButton){
+            buttons[idx] = cmd;
+            buttonpressed[idx] = false;
+        }
+    }
+
+    public void pressButton(int idx){
+        if(idx>=0 && idx < newButton && buttons[idx] != null){
+            if(!buttonpressed[idx]){
+                buttons[idx].execute();
+            }else{
+                buttons[idx].undo();
+            }
+            buttonpressed[idx] = !buttonpressed[idx];
+        }else{
+            System.out.println("button not set");
+        }
+    }
+
+}
+
+
+//commandDesign.java
+public class commandDesign{
+    public static void main(String[] args){
+        light livingLight = new light();
+        fan ceilingFan = new fan();
+
+        remoteContoller remote = new remoteContoller();
+
+        remote.setCommand(0,new lightCommand(livingLight));
+        remote.setCommand(1, new fanCommand(ceilingFan));
+
+        System.out.println("-- toggling light button");
+        remote.pressButton(0);
+        remote.pressButton(0);
+
+        System.out.println("-- toggling of fan button --");
+        remote.pressButton(1);
+        remote.pressButton(1);
+
+        System.out.println("pressed the unassigned button");
+        remote.pressButton(2);
+    
+    }
+}
+
 
 
