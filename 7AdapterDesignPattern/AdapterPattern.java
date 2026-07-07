edd4593575cfd134a17a101afea4477ff2f9f4ed
VICKYJHA00 <try.vickyjha@gmail.com>
2026-07-07 11:27:45 +0530

    adapter design pattern

--- 7AdapterDesignPattern/AdapterPattern.java
+++ 7AdapterDesignPattern/AdapterPattern.java
@@ -1,9 +1,9 @@
-
 interface Reports {
     String getJsonData(String data);
 }
 
-class xmlDataProvider {
+class XmlDataProvider {
+
     String getXmlData(String data) {
         int sep = data.indexOf(":");
         String name = data.substring(0, sep);
@@ -11,50 +11,53 @@ class xmlDataProvider {
 
         return "<user>"
                 + "<name>" + name + "</name>"
-                + "<id>"   + id   + "</id>"
+                + "<id>" + id + "</id>"
                 + "</user>";
     }
 }
 
 class XmlDataProviderAdapter implements Reports {
 
-    private xmlDataProvider  xmlProvider;
+    private XmlDataProvider xmlProvider;
 
-    public XmlDataProviderAdapter(xmlDataProvider provider) {
+    public XmlDataProviderAdapter(XmlDataProvider provider) {
         this.xmlProvider = provider;
     }
 
-    
+    @Override
     public String getJsonData(String data) {
+
         String xml = xmlProvider.getXmlData(data);
 
         int startName = xml.indexOf("<name>") + 6;
         int endName = xml.indexOf("</name>");
-        String name = xml.substring(startName,endName);
+        String name = xml.substring(startName, endName);
 
         int startId = xml.indexOf("<id>") + 4;
         int endId = xml.indexOf("</id>");
-        String Id = xml.substring(startId,endId);
+        String id = xml.substring(startId, endId);
 
-        return "{\"name\":\"" + name + "\",\"id\":\"" + Id + "\"}";
+        return "{\"name\":\"" + name + "\",\"id\":\"" + id + "\"}";
     }
 }
 
 class Client {
-    public void getReport(Reports report,String rawData){
-        System.out.println("Processed Json" +
-         report.getJsonData(rawData)
 
-        );
+    public void getReport(Reports report, String rawData) {
+        System.out.println("Processed Json: " + report.getJsonData(rawData));
     }
 }
 
 public class AdapterPattern {
-    public static void main(String args[]){
-        xmlDataProvider provider = new xmlDataProvider();
+
+    public static void main(String[] args) {
+
+        XmlDataProvider provider = new XmlDataProvider();
+
         Reports adapter = new XmlDataProviderAdapter(provider);
-        
+
         Client client = new Client();
+
         client.getReport(adapter, "John:123");
     }
 }
